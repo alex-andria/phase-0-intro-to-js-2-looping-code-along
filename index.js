c15daa490a81ed1ad0df545a3143c5a863123ed1
Alexandria DAntonio <adantonio@wayne.edu>
2021-10-06 12:09:18 -0400

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(cardNames, eventName){
+
+    const newNames = [];
+
+    for(let i = 0; i < cardNames.length; i++){
+        newNames.push(`Thank you, ${cardNames[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newNames;
+
+}
+
+function countDown(num = 4){
+
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+
+}

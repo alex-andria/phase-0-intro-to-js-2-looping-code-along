08e926e4235c16b0085714216a73a9725f260d96
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2021-11-30 02:50:42 -0500

    commit tree 49caa389cc10f4f553fb48f38487d56ecd0b0f5a

--- index.js
+++ index.js
@@ -1,21 +1,65 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+let array = ["name1", "name2"];
 
-function writeCards(cardNames, eventName){
+function writeCards(array, event = "birthday"){
 
-    const newNames = [];
+    let newArray = [];
 
-    for(let i = 0; i < cardNames.length; i++){
-        newNames.push(`Thank you, ${cardNames[i]}, for the wonderful ${eventName} gift!`);
-    }
-    return newNames;
-
-}
-
-function countDown(num = 4){
-
-    while(num >= 0){
-        console.log(num);
-        num--;
+    for (let i = 0; i < array.length; i++){
+        newArray[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
     }
 
+    return newArray;
 }
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number--);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function writeCards(cardNames, eventName){
+
+//     const newNames = [];
+
+//     for(let i = 0; i < cardNames.length; i++){
+//         newNames.push(`Thank you, ${cardNames[i]}, for the wonderful ${eventName} gift!`);
+//     }
+//     return newNames;
+
+// }
+
+// function countDown(num = 4){
+
+//     while(num >= 0){
+//         console.log(num);
+//         num--;
+//     }
+
+// }
